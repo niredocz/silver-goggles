930947dacea2ecf12527d95d8985c5ffe0182664
niredocz <daffaaldzakian20@gmail.com>
2020-12-17 16:17:36 +0700

    add new model

---
+++ app/Models/Datasiswa_model.php
@@ -0,0 +1,34 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Datasiswa_model extends Model {
+
+    public function get_data_siswa() {
+        $data = $this->db->table('data_siswa');
+        $data->select('data_siswa.*, agama.nama_agama');
+        $data->join('agama', 'agama.id_agama = data_siswa.id_agama','left');
+        return $data->get();
+    }
+    
+    public function get_agama() {
+        $data = $this->db->table('agama');
+        return $data->get();
+    }
+
+    public function edit_data_siswa($data, $id) {
+        $query = $this->db->table('data_siswa')->update($data, array('nis' => $id));
+        return $query;
+    }
+
+    public function simpan_data_siswa($data) {
+        $query = $this->db->table('data_siswa')->insert($data);
+        return $query;
+    }
+
+    public function hapus_data_siswa($id) {
+        $query = $this->db->table('data_siswa')->delete(array('nis' => $id));
+        return $query;
+    }
+
+}
