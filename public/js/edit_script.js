3e9c9a4973591249e5cbd89c3a64b4091d5c8ff1
niredocz <daffaaldzakian20@gmail.com>
2020-12-19 15:51:10 +0700

    add delete function

--- public/js/edit_script.js
+++ public/js/edit_script.js
@@ -14,12 +14,22 @@ $(document).ready(function() {
         $('.nama_siswa').val(nama);
         // $('.tempat_tgl_lahir').val(ttl);
         $('.tempat_lahir').val(tempat);
-        $('.tanggal_lahir').val(tanggal)
+        $('.tanggal_lahir').val(tanggal);
         $('.asal-sekolah').val(asal_sekolah);
         $('.alamat').val(alamat);
         $('.jenis_kelamin').val(gender).trigger('change');
         $('.agama').val(agama).trigger('change');
         // Call Modal Edit
         $('#editModal').modal('show');
+    });
+
+    // get Delete Product
+    $('.btn-delete').on('click',function(){
+        // get data from button edit
+        const nisn = $(this).data('nisn');
+        // Set data to Form Edit
+        $('.nisn_siswa').val(nisn);
+        // Call Modal Edit
+        $('#deleteModal').modal('show');
     });
 });
