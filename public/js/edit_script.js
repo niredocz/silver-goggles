3b423c22f94bb42c0b0a0f9ef8f176af14446e69
niredocz <daffaaldzakian20@gmail.com>
2020-12-21 11:05:48 +0700

    add delete listener for edit form

--- public/js/edit_script.js
+++ public/js/edit_script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.btn-edit').on('click',function(){
+
         // get data from button edit
         const nisn = $(this).data('nisn');
         const nama = $(this).data('nama');
@@ -9,26 +10,30 @@ $(document).ready(function() {
         const alamat = $(this).data('alamat');
         const gender = $(this).data('gender');
         const agama = $(this).data('agama');
+
         // Set data to Form Edit
         $('.nisn_siswa').val(nisn);
         $('.nama_siswa').val(nama);
-        // $('.tempat_tgl_lahir').val(ttl);
         $('.tempat_lahir').val(tempat);
         $('.tanggal_lahir').val(tanggal);
-        $('.asal-sekolah').val(asal_sekolah);
+        $('.sekolah').val(asal_sekolah);
         $('.alamat').val(alamat);
         $('.jenis_kelamin').val(gender).trigger('change');
         $('.agama').val(agama).trigger('change');
+
         // Call Modal Edit
         $('#editModal').modal('show');
     });
 
     // get Delete Product
     $('.btn-delete').on('click',function(){
+
         // get data from button edit
         const nisn = $(this).data('nisn');
+
         // Set data to Form Edit
         $('.nisn_siswa').val(nisn);
+        
         // Call Modal Edit
         $('#deleteModal').modal('show');
     });
