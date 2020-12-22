03b5630ede9921c7ac286b6fabd71138ff229e6a
niredocz <daffaaldzakian20@gmail.com>
2020-12-22 16:08:21 +0700

    add table report and print button

--- app/Views/pages/report.php
+++ app/Views/pages/report.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title><?=$title?></title>
+  <title>Cetak Laporan - SMAN 01 Surakarta</title>
   <!-- base:css -->
   <link rel="stylesheet" href="<?= base_url('assets/vendors/mdi/css/materialdesignicons.min.css')?> ">
   <link rel="stylesheet" href="<?= base_url('assets/vendors/feather/feather.css')?> ">
@@ -62,19 +62,19 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('/tambah')?>">
+            <a class="nav-link" href="<?= base_url('/registrasi')?>">
               <i class="icon-square-plus menu-icon"></i>
               <span class="menu-title">Daftar Siswa Baru</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('/edit')?>">
+            <a class="nav-link" href="<?= base_url('/tabel-siswa')?>">
               <i class="icon-layout menu-icon"></i>
               <span class="menu-title">Tabel Data Siswa</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('/report')?>">
+            <a class="nav-link" href="<?= base_url('/cetak-laporan')?>">
               <i class="icon-paper menu-icon"></i>
               <span class="menu-title">Cetak Laporan</span>
             </a>
@@ -89,8 +89,40 @@
               <div class="card">
                 <div class="card-body">
                   <div class="col-sm-12 mb-4 mb-xl-0">
-                    <h4 class="font-weight-bold text-dark">Hi, welcome back!</h4>
-                    <p class="font-weight-normal mb-2 text-muted">APRIL 1, 2019</p>
+                    <h6 class="font-weight-bold text-dark">Cetak Laporan Data Siswa</h6>
+                    <p class="font-weight-normal mb-2 text-muted">Silahkan klik tombol <strong>Download PDF</strong> untuk mencetak laporan pada tabel dibawah ini dalam format PDF</p>
+                    <div class="d-flex flex-row-reverse bd-highlight">
+                      <!-- <button class="btn btn-primary pdf">Download PDF</button> -->
+                      <a href="<?php echo base_url('/pdf') ?>">Generate</a>
+                    </div>
+                    <div class="table-responsive">
+                      <table class="table table-striped table-hover" id="printContent">
+                        <thead>
+                          <tr>
+                            <th class="text-center">NISN</th>
+                            <th class="text-center">Nama Lengkap</th>
+                            <th class="text-center">Tempat/Tanggal Lahir</th>
+                            <th class="text-center">Asal Sekolah</th>
+                            <th class="text-center">Alamat Tinggal</th>
+                            <th class="text-center">Jenis Kelamin</th>
+                            <th class="text-center">Agama</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php foreach($data_siswa as $row): ?>
+                          <tr>
+                            <td><?=$row->nisn;?></td>
+                            <td><?=$row->nama;?></td>
+                            <td><?=$row->tempat_lahir .', '. date('d-m-Y', strtotime($row->tanggal_lahir))?></td>
+                            <td><?=$row->asal_sekolah;?></td>
+                            <td><?=$row->alamat;?></td>
+                            <td><?=$row->nama_gender;?></td>
+                            <td><?=$row->nama_agama;?></td>
+                          </tr>
+                          <?php endforeach; ?>
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -113,21 +145,20 @@
   <!-- container-scroller -->
 
   <!-- base:js -->
+  <script src="<?= base_url('js/jquery.min.js')?> "></script>
+  <script src="<?= base_url('js/bootstrap.min.js')?> "></script>
   <script src="<?= base_url('assets/vendors/base/vendor.bundle.base.js')?> "></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <!-- End plugin js for this page-->
   <!-- inject:js -->
   <script src="<?= base_url('assets/js/off-canvas.js')?> "></script>
   <script src="<?= base_url('assets/js/hoverable-collapse.js')?> "></script>
   <script src="<?= base_url('assets/js/template.js')?> "></script>
-  <!-- endinject -->
-  <!-- plugin js for this page -->
-  <script src="<?= base_url('assets/vendors/chart.js/Chart.min.js')?> "></script>
-  <script src="<?= base_url('assets/vendors/jquery-bar-rating/jquery.barrating.min.js')?> "></script>
-  <!-- End plugin js for this page -->
+
   <!-- Custom js for this page-->
-  <script src="<?= base_url('assets/js/dashboard.js')?> "></script>
+  <!-- <script src="https://code.jquery.com/jquery-1.12.4.js"></script> -->
+  <script src="<?= base_url('assets/jquery-ui/jquery-ui.min.js')?> "></script>
+  <script src="<?= base_url('js/datepicker-id.js')?>" type="text/javascript"></script>
+  <script src="<?= base_url('js/script.js')?>"></script>
+  
   <!-- End custom js for this page-->
 </body>
 
